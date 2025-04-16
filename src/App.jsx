af0fc989f30cd18428a5567194041e8812737997
Ivan <chaca360@gmail.com>
2025-04-16 16:56:07 -0500

    proyecto en blanco

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>        
+      <p className="read-the-docs">
+        proyecto en blanco
+      </p>
+    </>
+  )
+}
+
+export default App
